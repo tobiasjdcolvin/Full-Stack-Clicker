28db79bd29a05f55b2259e8355fea55029ac6e92
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2024-11-19 14:13:23 -0700

    Added starter code for testing that the database is working, added three initial users

--- russ_clicker/backend/server.js
+++ russ_clicker/backend/server.js
@@ -1,26 +1,59 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const mongoURL = "mongodb://127.0.0.1/russ_clicker_db";
 const app = express();
 const port = 50001;
-let USERNAME = "";
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    score: Number,
+    multiplier: Number,
+    upgrades: Object,
+});
+const UserObj = mongoose.model("UserObj", UserSchema);
+
+// connect to database and initialize stuff
+async function main() {
+    await mongoose.connect(mongoURL);
+
+    // These will be the initial users stored in database,
+    // all other users will need to be registered. This is so
+    // we developers can test that everything is up and running
+
+    let tobinskiExists = await UserObj.exists({ username: "Tobinski" });
+    // if Tobinski exists, that means Slurricane and Dracian also exist, and the
+    // opposite is true too
+    if (!tobinskiExists) {
+        let tobinskiUser = new UserObj({
+            username: "Tobinski",
+            score: 0,
+            multiplier: 0,
+            upgrades: {},
+        });
+        let slurricaneUser = new UserObj({
+            username: "Slurricane",
+            score: 0,
+            multiplier: 0,
+            upgrades: {},
+        });
+        let dracianUser = new UserObj({
+            username: "Dracian",
+            score: 0,
+            multiplier: 0,
+            upgrades: {},
+        });
+        await tobinskiUser.save();
+        await slurricaneUser.save();
+        await dracianUser.save();
+    }
+
+
+}
+main();
 
 
 
-
-// WITHIN THE LINES WILL BE REPLACED BY DATABASE
-// ------------------------------------------------------------------------------------
-let SCORE = { // TEMPORARY - will retrieve from database in future, using USERNAME
-    "username": 0,
-    "Tobinski": 0,
-    "Slurricane": 0,
-};
-let testNamesDatabase = ["username", "Tobinski", "Slurricane"] // TEMPORARY - will be replaced with requests to database 
-// ------------------------------------------------------------------------------------
-
-
-
-
-// statically serves files within public_html directory
-app.use(express.static("../frontend/public_html"));
+/*
 
 // auto-increments scores for those who have the upgrade
 setInterval(() => {
@@ -29,7 +62,6 @@ setInterval(() => {
     // here, lets just say Slurricane has the upgrade
     SCORE["Slurricane"] += 1; // this will be request to database too
 }, 1000);
-
 
 // API Endpoints below:
 
@@ -66,6 +98,8 @@ app.get("/getScore/:username", (req, res) => {
     res.statusCode = 200;
     res.send(`${playerScore}`);
 })
+
+*/
 
 
 app.listen(port, () => {
